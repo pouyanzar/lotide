3879a56109706b3a35c4ee92f8e6ccc005585258
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-07-26 20:18:50 +0000

    eqArrays refactored and test file added

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} ==== ${expected}`)
-    : console.log(`🔴🔴🔴 Assertation Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -15,7 +10,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual");
-
 const tail = function(arr) {
   if (arr.length === 1) {
     return [];
